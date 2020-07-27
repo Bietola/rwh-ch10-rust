449b1067cc1a9c3b057b10c67fbc6fbfd18b251b
Bietola <dincio.montesi@gmail.com>
2020-07-27 16:48:32 +0200

    Implement `get_num`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use bstr::ByteSlice;
 use std::error::Error;
 use std::io::Read;
+use std::cmp::Ordering;
+
+#[derive(PartialEq, Debug)]
+enum ParseErr {
+    NoValidFieldLeft,
+    NoHeaderMatch,
+    Utf8Error(bstr::Utf8Error),
+    InvalidNum(std::num::ParseIntError),
+}
 
 struct PGM {
     width: usize,
@@ -9,10 +19,10 @@ struct PGM {
 }
 
 type ParseInput<'a> = &'a [u8];
-type ParseResult<'a, T> = Result<(T, ParseInput<'a>), &'static str>;
+type ParseResult<'a, T> = Result<(T, ParseInput<'a>), (ParseErr, ParseInput<'a>)>;
 
 fn parse_pgm(input: ParseInput) -> ParseResult<PGM> {
-    let (_, rest) = match_header(input)?;
+    let (_, rest) = match_header_version(input)?;
 
     let (width, rest) = get_num(rest)?;
     let (height, rest) = get_num(rest)?;
@@ -20,24 +30,54 @@ fn parse_pgm(input: ParseInput) -> ParseResult<PGM> {
     let (contents, rest) = get_bits(rest, width * height)?;
 
     Ok((
-        PGM {
-            width: width as usize,
-            height: height as usize,
-            max_grey_val: max_grey_val as u8,
-            contents,
-        },
-        rest,
+            PGM {
+                width: width as usize,
+                height: height as usize,
+                max_grey_val: max_grey_val as u8,
+                contents,
+            },
+            rest,
     ))
 }
 
-fn match_header(input: ParseInput) -> ParseResult<()> {
-    if input.split("hello") {
+fn match_header_version(input: ParseInput) -> ParseResult<()> {
+    const VERSION_STR: &str = "P5";
 
+    if input.starts_with_str(VERSION_STR) {
+        // +1 is for the `\n` after the VERSION_STR
+        let read_until = VERSION_STR.len() + 1;
+
+        Ok(((), &input[read_until..]))
+    } else {
+        Err((ParseErr::NoHeaderMatch, input))
     }
 }
 
 fn get_num(input: ParseInput) -> ParseResult<i32> {
-    unimplemented!()
+    let raw_num_str = match input.fields().next() {
+        Some(s) => s,
+        None => return Err((ParseErr::NoValidFieldLeft, input)),
+    };
+
+    let num = match raw_num_str.to_str() {
+        Ok(s) => match s.parse::<i32>() {
+            Ok(s) => s,
+            Err(er) => return Err((ParseErr::InvalidNum(er), input)),
+        },
+        Err(er) => return Err((ParseErr::Utf8Error(er), input)),
+    };
+
+    // `parsed_len` is length to consume after parse. The comparison is for "end of string" edge
+    // case.
+    let len = raw_num_str.len();
+    let parsed_len = match len.cmp(&input.len()) {
+        Ordering::Greater => panic!("Paradoxically parsed beyond string end"),
+        Ordering::Equal => len,
+        // +1 for skipping next whitespace (there should be always one is PGM specification)
+        Ordering::Less => len + 1,
+    };
+
+    Ok((num, &input[parsed_len..]))
 }
 
 fn get_bits(input: ParseInput, amount: i32) -> ParseResult<Vec<bool>> {
@@ -53,4 +93,35 @@ fn main() -> Result<(), Box<dyn Error>> {
     let pgm = parse_pgm(bstr::BString::from(contents).as_slice());
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn match_header_version_obv() {
+        let mock_header: &[u8] = indoc!("P5
+            120 32")
+            .as_bytes();
+
+        assert_eq!(
+            match match_header_version(mock_header) {
+                Ok(((), s)) => Ok(((), s.as_bstr())),
+                Err((er, s)) => Err((er, s.as_bstr())),
+            },
+            Ok(((), "120 32".as_bytes().as_bstr())),
+        );
+    }
+
+    #[test]
+    fn get_num_single() {
+        assert_eq!(get_num("12".as_bytes()), Ok((12, "".as_bytes())));
+    }
+
+    #[test]
+    fn get_num_multiple() {
+        assert_eq!(get_num("12 24".as_bytes()), Ok((12, "24".as_bytes())));
+    }
 }
